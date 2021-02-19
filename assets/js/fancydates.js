51dd6068a596c2497a577ff4e1ee7aafe9b413cc
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2021-02-19 16:51:06 -0500

    Nikola auto commit.
    Source commit: ce89953525cbe6e06f109a1fd34b2008068a9838
    Nikola version: 8.1.3

--- assets/js/fancydates.js
+++ assets/js/fancydates.js
@@ -1,1 +1,22 @@
-function fancydates(fanciness,date_format){if(fanciness==0){return}var dates=document.querySelectorAll(".dt-published, .dt-updated, .listdate");var l=dates.length;for(var i=0;i<l;i++){var d=moment(dates[i].attributes.datetime.value);var o;if(fanciness==1){o=d.local().format(date_format)}else{o=d.fromNow()}dates[i].innerHTML=o}};
+function fancydates(fanciness, luxonDateFormat) {
+    if (fanciness === 0) {
+        return;
+    }
+
+    var dates = document.querySelectorAll('.dt-published, .dt-updated, .listdate');
+
+    var l = dates.length;
+
+    for (var i = 0; i < l; i++) {
+        var d = luxon.DateTime.fromISO(dates[i].attributes.datetime.value);
+        var o;
+        if (fanciness === 1 && luxonDateFormat.preset) {
+            o = d.toLocal().toLocaleString(luxon.DateTime[luxonDateFormat.format]);
+        } else if (fanciness === 1) {
+            o = d.toLocal().toFormat(luxonDateFormat.format);
+        } else {
+            o = d.toRelative();
+        }
+        dates[i].innerHTML = o;
+    }
+}
