c7b524e591fbd96b8b581f05cc3c62008d44838b
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2021-09-19 23:16:06 -0400

    Nikola auto commit.
    Source commit: aeb6d833a5d40fb1e91b987a507c698cbe16a7f5
    Nikola version: 8.1.3

---
+++ assets/js/add_styles.js
@@ -0,0 +1,33 @@
+// Because we can't add classes the way Blaze would like, element by element, we chuck it all in here.
+// Blaze UI
+// $("p").addClass("c-paragraph");
+// $("a").addClass("c-link");
+
+// $("code, pre").addClass("u-text--mono");
+// $("code, pre").addClass("u-small");
+// $("kbd").addClass("u-kbd");
+
+// $("h1, h2, h3, h4, h5, h6").addClass("c-heading");
+// $("h1").addClass("u-super");
+// $("h2").addClass("u-xlarge");
+// $("h3").addClass("u-large");
+// $("h4").addClass("u-medium");
+// $("h5").addClass("u-small");
+// $("h6").addClass("u-xsmall");
+
+// $("table").addClass("c-table");
+// $("table").addClass("c-table--striped");
+// $("thead").addClass("c-table__head");
+// $("tbody").addClass("c-table__body");
+// $("thead tr").addClass("c-table__row--head");
+// $("tr").addClass("c-table__row");
+// $("th, td").addClass("c-table__cell");
+
+// Halfmoon
+
+$("table").addClass("table");
+$("table").addClass("table-striped");
+
+$("h2").addClass("content-title");
+
+$("code").addClass("code");
